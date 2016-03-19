d484b9f35a8ec0c296a27e065ae5a24237b769d4
Dillon Drenzek <dr.enzek@gmail.com>
2016-03-19 15:17:29 -0700

    resolves #3 - stylus compiles via gulp

---
+++ app/palette.ts
@@ -0,0 +1,11 @@
+import { Component } from 'angular2/core';
+
+@Component({
+	selector:'app',
+	templateUrl: 'app/palette.html',
+	styleUrls: ['built/css/palette.css']
+})
+
+export class Palette {
+	appTitle: string = "Palette";
+}
